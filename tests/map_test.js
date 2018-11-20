d852c32c724d0b7366a772d5037ef366cdfd7621
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-11-20 14:25:56 +0530

    Add more tests for map function.

--- tests/map_test.js
+++ tests/map_test.js
@@ -10,3 +10,7 @@ const testMap = function(mapper, input, output) {
 };
 
 testMap(square, [1], [1]);
+testMap(square, [0], [0]);
+testMap(square, [], []);
+testMap(square, [2], [4]);
+testMap(square, [1,2,3,4,5], [1,4,9,16,25]);
